56d88cf06746927e308f557584670eb8fe359499
bemnet884 <155265449+bemnet884@users.noreply.github.com>
2025-07-26 12:08:58 +0300

    again

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"T+Tw2tQaflt2370emceN2ZsZ/tnQD6AfkfhgXvFh0nk=\"\n}"
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="underline text-4xl">
+      hello
+    </div>
+  );
+}
